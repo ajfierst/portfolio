3ca88e77e17684546f06a65e72e89fae47828a37
ajfierst <65756738+ajfierst@users.noreply.github.com>
2024-04-09 22:19:35 -0700

    Updated portfolio projects and biography

---
+++ build/precache-manifest.dffa6dee632c941be8e0f877890441ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f480b35bf12eefb97e9",
+    "url": "/afierst22/portfolio/static/css/main.9a4cee65.chunk.css"
+  },
+  {
+    "revision": "1f480b35bf12eefb97e9",
+    "url": "/afierst22/portfolio/static/js/main.cc1e6ac8.chunk.js"
+  },
+  {
+    "revision": "1233fdc190686fc23936",
+    "url": "/afierst22/portfolio/static/js/runtime~main.84bed956.js"
+  },
+  {
+    "revision": "6738d16c3e31c36a5680",
+    "url": "/afierst22/portfolio/static/js/2.80c29fff.chunk.js"
+  },
+  {
+    "revision": "4f48a5bd49a5c97f8c05779f8b924217",
+    "url": "/afierst22/portfolio/index.html"
+  }
+];
